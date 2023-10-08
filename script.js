f6a1bbd07455ead851306309418ec7b714e192ba
Ashutosh Ukale <123993522+ashutoshukale@users.noreply.github.com>
2023-10-08 12:00:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var reload = confirm("are you sure you want to leave?");
+if(reload)
+{
+    location.reload(); // Refresh the site, not needed really.
+    location.href = "http://example.com"; // Redirect.
+    //password:st3gisamazing
+}
